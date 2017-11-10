4d9d697119fc9adad280843318c98d218c3726d6
Joshua Vial <joshua@enspiral.com>
2017-11-10 15:57:08 +1300

    adding instructions to block

--- react-app/src/Block.js
+++ react-app/src/Block.js
@@ -3,31 +3,41 @@ import FontAwesome from 'react-fontawesome'
 import {Link} from 'react-router-dom'
 import {findBlock} from './content/parser'
 import './Block.css'
+import './font-awesome.min.css'
 
 const Block = (props) => {
   let block = findBlock(props.match.params.block_id)
-  return (
-    <div className='Block'>
-      <div id='title'>
-        {block.title}
-      </div>
-      <div id='nav'>
-        <FontAwesome name='caret-left' size='2x'/>
-        <span id='label'>
-          Instructions
-        </span>
-        <FontAwesome name='caret-right' size='2x'/>
-      </div>
-      <div id='content'>
-        <ul>
-          {block.instructions.map((instruction, index) => <li key={index}>{instruction}</li>)}
-        </ul>
-        <div id='notes'>{block.notes}</div>
-        <Link className='button' to='/'>Continue</Link>
-      </div>
-
-    </div>
+  let content = <div>
+    <ul>
+      {block.instructions.map((instruction, index) => <li key={index}>{instruction}</li>)}
+    </ul>
+    <div id='notes'>{block.notes}</div>
+    <Link className='button' to='/menu'>Continue</Link>
+  </div>
+  return renderBlock({
+    title: block.title,
+    next: '/menu',
+    prev: '/menu',
+    label: 'Instructions',
+    content
+  }
   )
+}
+
+function renderBlock({title, next, prev, label, content}) {
+    return <div className='Block'>
+      <div id='title'> {title} </div>
+      <div id='nav'>
+        <Link to={prev}>
+          <FontAwesome name='caret-left' size='2x'/>
+        </Link>
+        <span id='label'> {label} </span>
+        <Link to={next}>
+          <FontAwesome name='caret-right' size='2x'/>
+        </Link>
+      </div>
+      <div id='content'> {content}</div>
+    </div>
 }
 
 export default Block
