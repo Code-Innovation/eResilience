b4954526641d6cfdb26976ac9550aa68501d0832
Joshua Vial <joshua@enspiral.com>
2017-11-10 17:04:00 +1300

    completion exercise

--- react-app/src/Block.js
+++ react-app/src/Block.js
@@ -14,6 +14,13 @@ const Block = ({match}) => {
     blockContent = blockInstructions(block)
   } else if (!params.step_id) {
     blockContent = exerciseObjective(params.exercise_id, block)
+  } else {
+    console.log(params)
+    if (params.step_id === 'completion') {
+      blockContent = completionStep(params.exercise_id, block)
+    } else {
+      blockContent = contentForStep(params.step_id, params.exercise_id, block)
+    }
   }
   return renderBlock(blockContent)
 }
@@ -56,11 +63,51 @@ function exerciseObjective(exercise_id, block) {
   let exercise = block.exercises.find(e => e.id === exercise_id)
   return {
     title: exercise.title,
-    next: '/',
+    next: `/block/${block.id}/${exercise.id}/0`,
     prev: `/block/${block.id}`,
     label: 'Objective',
     content: exercise.objective,
     contentClass: 'highlight'
+  }
+}
+
+function contentForStep(step_id, exercise_id, block) {
+  step_id = Number(step_id)
+  let exercise = block.exercises.find(e => e.id === exercise_id)
+
+  let blockPath = `/block/${block.id}`
+  let exercisePath = `${blockPath}/${exercise_id}`
+
+  let prev = step_id === 0 ? exercisePath : `${exercisePath}/${step_id-1}`
+
+  let next
+  let isLastStep = step_id === exercise.steps.length-1
+  if (isLastStep && exercise.completion) {
+    next = `${exercisePath}/completion`
+  } else  {
+    next = isLastStep ? blockPath : `${exercisePath}/${step_id + 1}`
+  }
+
+  let content = exercise.steps[step_id]
+  return {
+    title: exercise.title,
+    next,
+    prev,
+    label: `Step ${step_id + 1} of ${exercise.steps.length}`,
+    content
+  }
+}
+
+function completionStep(exercise_id, block) {
+  let exercise = block.exercises.find(e => e.id === exercise_id)
+  let prev = `/block/${block.id}/${exercise_id}/${exercise.steps.length-1}`
+  let next = `/block/${block.id}`
+  return {
+    title: exercise.title,
+    next,
+    prev,
+    label: 'Group Participation',
+    content: exercise.completion
   }
 }
 
